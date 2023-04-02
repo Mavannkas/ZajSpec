c9469a6d43fb7d2e94a2dd4b46c5249f572ba3e6
Mavannkas <wiktor.ludwiniak@gmail.com>
2023-04-02 15:39:57 +0200

    Initial commit

---
+++ src/main/java/com/example/zajspec/ZajSpecApplication.java
@@ -0,0 +1,13 @@
+package com.example.zajspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZajSpecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZajSpecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zajspec/ZajSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zajspec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZajSpecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
